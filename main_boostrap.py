9a3fef86051b6a83bc6e75773ee92ab081152c9f
Prem-jun <prem@kku.ac.th>
2025-01-27 15:08:22 +0700

    update all

--- main_boostrap.py
+++ main_boostrap.py
@@ -16,6 +16,9 @@ import numpy as np
 import lib_boostrap
 from typing import List, Union, Dict
 from dataclasses import dataclass, field
+from pathlib import Path
+import argparse
+import yaml
 
 @dataclass
 class Res_boostrap:
@@ -65,32 +68,188 @@ def read_json_file(file_path):
         data = json.load(file)  # Load the data from the file
     return data
      
-def main(folder_path:str ,filename:str):
+# def main(folder_path:str ,filename:str):
     
-    # Define the path to the JSON file
-    filetype_re = '.pkl'
-    filetype_samp = '.json'
+#     # Define the path to the JSON file
+#     filetype_re = '.pkl'
+#     filetype_samp = '.json'
     
-    # filepath_pop = os.path.join(folder_path,filename+filetype_pop)
-    filepath_samp = os.path.join(folder_path,filename+filetype_samp)
+#     # filepath_pop = os.path.join(folder_path,filename+filetype_pop)
+#     filepath_samp = os.path.join(folder_path,filename+filetype_samp)
     
-    # Read sample file
-    file_path = filepath_samp
-    # fold_re = filename+'_re'
+#     # Read sample file
+#     file_path = filepath_samp
+#     # fold_re = filename+'_re'
     
-    # file_path_re = os.path.join(folder_path,fold_re) 
-    res_all = []
-    try:
-        # pop_data =  pd.read_pickle(filepath_pop)
-        # pop_min = np.min(pop_data)
-        # pop_max = np.max(pop_data)
-        # pop_range = pop_max-pop_min
-        json_data = read_json_file(file_path)
-        for count, data in enumerate(json_data):
+#     # file_path_re = os.path.join(folder_path,fold_re) 
+#     res_all = []
+#     try:
+#         # pop_data =  pd.read_pickle(filepath_pop)
+#         # pop_min = np.min(pop_data)
+#         # pop_max = np.max(pop_data)
+#         # pop_range = pop_max-pop_min
+#         json_data = read_json_file(file_path)
+#         for count, data in enumerate(json_data):
             
+#             # chunk_size.append(data['chunk_size'])
+#             chunk_data = data['samp_chuck']
+            
+#             # create the initial network
+#             net_online = lib_boostrap.booststream()
+#             net_online_mm = lib_boostrap.booststream()
+#             net_online_cum = lib_boostrap.booststream()
+#             net_trad_cum = lib_boostrap.booststream()
+            
+#             # Setting for online manner
+#             net_online.set_online()
+#             net_online_mm.set_online(minmax_flag=True)
+#             net_online_cum.set_online()
+            
+#             # Setting results 
+#             res4 = Res_boostrap()
+#             res4.add_init_params(net = net_trad_cum)
+#             res1 = Res_boostrap()
+#             res1.add_init_params(net = net_online)
+#             res2 = Res_boostrap()
+#             res2.add_init_params(net = net_online_mm)
+#             res3 = Res_boostrap()
+#             res3.add_init_params(net = net_online_cum,cum=True)
+#             sample_whole = []
+#             count2 = 0 
+#             for samples_chunk in chunk_data:
+#                 count2 +=1
+#                 sample_whole = sample_whole + samples_chunk
+#                 # Train online chunk
+#                 print(f'Online running round:{count}/{count2}')
+#                 expandsion = net_online.expand_bt_online(samples_chunk)
+#                 res1.add_params(net_online)
+#                 print(f'Online minmax running round:{count}/{count2}')
+#                 expandsion_mm = net_online_mm.expand_bt_online(samples_chunk)
+#                 res2.add_params(net_online_mm)
+#                 # print(f'Online whole running round:{count}/{count2}')
+#                 # expansion_ocum = net_online_cum.expand_bt_online(sample_whole,cum=True)
+#                 # res3.add_params(net_online_cum)
+#                 print(f'Offline running round:{count}/{count2}')
+#                 expansion_tcum = net_trad_cum.expand_bt_trad(sample_whole)
+#                 res4.add_params(net_trad_cum)
+            
+#             res_all.append(res1)
+#             res_all.append(res2)
+#             # res_all.append(res3)
+#             res_all.append(res4)
+#             print(f'Round running:{count}')
+#         data_wb = {
+#                 'result_all': res_all
+#             }
+#         # Specify the name of the pickle file
+#         pickle_file = os.path.join(folder_path,filename+'_re.pkl')
+#         # Save the variables to the pickle file
+#         with open(pickle_file, 'wb') as file:
+#             pickle.dump(data_wb, file)
+#         # print(json_data)  # Print the contents of the JSON file
+#     except FileNotFoundError:
+#         print(f"The file {file_path} was not found.")
+#     except json.JSONDecodeError:
+#         print(f"The file {file_path} is not a valid JSON.")
+    
+    
+#     #return 0
+            
+# if __name__=='__main__':
+#     dist_list = ['normal','wald','wiebull','fdist','realworld']
+#     idx = 4
+#     dist_select = dist_list[idx]
+#     # dist_select = 'realworld'
+#     # ====== program part
+#     if dist_select == 'realworld':
+#         # normal distribution
+#         folder_path = './config_sim_data/realworld/'
+#         # filename_list = ['laptop_prices','Electronic_sales_Sep2023-Sep2024',
+#         #                  'Ecommerce_Sales_Prediction_Dataset','world_tourism_economy_data']
+#         filename_list = ['world_tourism_economy_data']
+    
+#     if dist_select == 'fdist':
+#         # normal distribution
+#         folder_path = './config_sim_data/fdist/'
+#         # filename_list = ['normalm0sd1n10000','normalm0sd25n10000',
+#         #                  'normalm0sd25n50000','normalm0sd100n50000']
+#         filename_list = ['fdistdfn5dfd10n10000','fdistdfn5dfd15n10000','fdistdfn5dfd20n10000']
+    
+#     if dist_select == 'normal':
+#         # normal distribution
+#         folder_path = './config_sim_data/normal/'
+#         # filename_list = ['normalm0sd1n10000','normalm0sd25n10000',
+#         #                  'normalm0sd25n50000','normalm0sd100n50000']
+#         filename_list = ['normalm0sd1n10000','normalm0sd4n10000']
+#         # ======
+#     if dist_select == 'wiebull':
+#         # wiebull distribution
+#         folder_path = './config_sim_data/wiebull/'
+#         # filename_list = ['wiebullshape5n50000']
+#         filename_list = ['wiebullshape1n10000','wiebullshape5n10000']
+        
+#         # ======
+#     if dist_select == 'wald':    
+#         # wald distribution
+#         folder_path = './config_sim_data/wald/'
+#         filename_list = ['waldm1sd2n10000','waldm1sd05n10000']
+#         # filename_list = ['waldm1sd2n10000','waldm1sd05n10000',
+#         #             'waldm3sd2n10000','waldm3sd05n10000','waldm3sd05n10000']
+#         # filename_list = ['waldm1sd2n10000','waldm1sd2n50000','waldm1sd05n10000','waldm1sd05n50000',
+#         #             'waldm3sd2n10000','waldm3sd2n50000','waldm3sd05n10000','waldm3sd05n10000']
+#         # ======
+#     print(f"Your selected distribution is: {dist_select}")
+#     flag = input("Running y/n: ")
+#     if (flag == 'y') or (flag == 'Y'):    
+#         for filename in filename_list:
+#             main(folder_path,filename)
+#     else:
+#         print(f'Please, change the selected distribution')    
+
+def parse_opt():
+    """
+    Args:
+        --source (str | list[str], optional): Configure file results parth. Defaults to ROOT /'config_sim_data/config_results_normal.yaml'.
+        
+    Returns:
+        argparse.Namespace: Parsed command-line arguments as an argparse.Namespace object.
+
+    Example:
+        ```python
+        
+        ```
+    """
+    parser = argparse.ArgumentParser(
+        description='sample chunks generator',
+        epilog='Example: python sim_data_samp_chunk.py --dir config_sim_data/uniform --file config_uniform_simulate.yaml',  # ข้อความท้าย help
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,  # แสดง default values
+        prog='Sample generator-Tool'
+        )
+    ROOT = Path(__file__).parent
+    parser.add_argument("--dir", type = str, default=ROOT/"config_sim_data/uniform", help = 'working directory')
+    parser.add_argument("--file", type = str, default="config_uniform_simulate.yaml", help = 'config file')
+    opt = parser.parse_args()
+    return opt
+
+def read_yaml_config(file_path):
+    """
+    Read YAML configuration file that contains multiple documents separated by '---'
+    Returns a list of configuration dictionaries
+    """
+    with open(file_path, 'r') as file:
+        # yaml.safe_load_all returns a generator of all documents in the YAML file
+        configs = list(yaml.safe_load_all(file))
+    return configs
+
+def run(dir:str,file:str):
+    yaml_path = os.path.join(dir,file)
+    configs = read_yaml_config(yaml_path)
+    for config in configs:
+        res_all = []
+        json_data = read_json_file(os.path.join(dir,config['file_data_chunk']+'.json'))
+        for count, data in enumerate(json_data):
             # chunk_size.append(data['chunk_size'])
             chunk_data = data['samp_chuck']
-            
             # create the initial network
             net_online = lib_boostrap.booststream()
             net_online_mm = lib_boostrap.booststream()
@@ -139,66 +298,16 @@ def main(folder_path:str ,filename:str):
                 'result_all': res_all
             }
         # Specify the name of the pickle file
-        pickle_file = os.path.join(folder_path,filename+'_re.pkl')
+        pickle_file = os.path.join(dir,config['file_data_chunk']+'_re.pkl')
         # Save the variables to the pickle file
         with open(pickle_file, 'wb') as file:
             pickle.dump(data_wb, file)
         # print(json_data)  # Print the contents of the JSON file
-    except FileNotFoundError:
-        print(f"The file {file_path} was not found.")
-    except json.JSONDecodeError:
-        print(f"The file {file_path} is not a valid JSON.")
-    
-    
-    #return 0
-            
-if __name__=='__main__':
-    dist_list = ['normal','wald','wiebull','fdist','realworld']
-    idx = 4
-    dist_select = dist_list[idx]
-    # dist_select = 'realworld'
-    # ====== program part
-    if dist_select == 'realworld':
-        # normal distribution
-        folder_path = './config_sim_data/realworld/'
-        # filename_list = ['laptop_prices','Electronic_sales_Sep2023-Sep2024',
-        #                  'Ecommerce_Sales_Prediction_Dataset','world_tourism_economy_data']
-        filename_list = ['world_tourism_economy_data']
-    
-    if dist_select == 'fdist':
-        # normal distribution
-        folder_path = './config_sim_data/fdist/'
-        # filename_list = ['normalm0sd1n10000','normalm0sd25n10000',
-        #                  'normalm0sd25n50000','normalm0sd100n50000']
-        filename_list = ['fdistdfn5dfd10n10000','fdistdfn5dfd15n10000','fdistdfn5dfd20n10000']
-    
-    if dist_select == 'normal':
-        # normal distribution
-        folder_path = './config_sim_data/normal/'
-        # filename_list = ['normalm0sd1n10000','normalm0sd25n10000',
-        #                  'normalm0sd25n50000','normalm0sd100n50000']
-        filename_list = ['normalm0sd1n10000','normalm0sd4n10000']
-        # ======
-    if dist_select == 'wiebull':
-        # wiebull distribution
-        folder_path = './config_sim_data/wiebull/'
-        # filename_list = ['wiebullshape5n50000']
-        filename_list = ['wiebullshape1n10000','wiebullshape5n10000']
         
-        # ======
-    if dist_select == 'wald':    
-        # wald distribution
-        folder_path = './config_sim_data/wald/'
-        filename_list = ['waldm1sd2n10000','waldm1sd05n10000']
-        # filename_list = ['waldm1sd2n10000','waldm1sd05n10000',
-        #             'waldm3sd2n10000','waldm3sd05n10000','waldm3sd05n10000']
-        # filename_list = ['waldm1sd2n10000','waldm1sd2n50000','waldm1sd05n10000','waldm1sd05n50000',
-        #             'waldm3sd2n10000','waldm3sd2n50000','waldm3sd05n10000','waldm3sd05n10000']
-        # ======
-    print(f"Your selected distribution is: {dist_select}")
-    flag = input("Running y/n: ")
-    if (flag == 'y') or (flag == 'Y'):    
-        for filename in filename_list:
-            main(folder_path,filename)
-    else:
-        print(f'Please, change the selected distribution')        
+    
+def main(opt):
+    run(**vars(opt))
+    
+if __name__ == "__main__":
+    opt = parse_opt()
+    main(opt)    
--- sim_data_samp_chunk.py
+++ sim_data_samp_chunk.py
@@ -128,18 +128,19 @@ def parse_opt():
         ```
     """
     parser = argparse.ArgumentParser(
-        description='Population simulation',
-        epilog='Example: python script.py --dir config_sim_data --file config_uniform.yaml',  # ข้อความท้าย help
+        description='sample chunks generator',
+        epilog='Example: python sim_data_samp_chunk.py --dir config_sim_data/uniform --file config_uniform_simulate.yaml',  # ข้อความท้าย help
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,  # แสดง default values
-        prog='Population generator-Tool'
+        prog='Sample generator-Tool'
         )
     ROOT = Path(__file__).parent
-    parser.add_argument("--dir", type = str, default=ROOT/"config_sim_data/normal", help = 'working directory')
-    parser.add_argument("--file", type = str, default="config_normal_simulate.yaml", help = 'config file')
+    parser.add_argument("--dir", type = str, default="./config_sim_data/uniform", help = 'working directory')
+    parser.add_argument("--file", type = str, default="config_uniform_simulate.yaml", help = 'config file')
+    parser.add_argument("--pfeed", type = int, default=30, help = 'percent of samples out of population')
     opt = parser.parse_args()
     return opt
 
-def run(dir:str,file:str):
+def run(dir:str,file:str,pfeed:int,ch_size:List = [50,500]):
     pathfile = os.path.join(dir,file)
     with open(pathfile,'r') as f:
         docs = yaml.safe_load_all(f)
@@ -149,19 +150,20 @@ def run(dir:str,file:str):
             list_filename.append(doc['file_data_chunk'])
     
     for file in list_filename:
-        pop_sim = pd.read_pickle(os.path.join(folder_config,file+'.pkl'))
+        pop_sim = pd.read_pickle(os.path.join(dir,file+'.pkl'))
         samp_list = []
-        for size in chunk_size:
-            samp_list.append(samp1d(file_config=file_config, nsim=len(pop_sim),name = file, 
-                               percent_feed=percent_feed))
+        for size in ch_size:
+            samp_list.append(samp1d(file_config=dir, nsim=len(pop_sim),name = file, 
+                               percent_feed=pfeed))
             samp_list[-1].split2chunk(pop_sim,size) 
         samp_list_dict = [asdict(samp) for samp in samp_list]  
         samp_json = json.dumps(samp_list_dict, indent=4)
-        with open(os.path.join(folder_config,file+'.json'), 'w') as json_file:
+        with open(os.path.join(dir,file+'.json'), 'w') as json_file:
             json_file.write(samp_json)  
     return 0
 def main(opt):
     run(**vars(opt))
+    
 if __name__ == "__main__":
     opt = parse_opt()
     main(opt)
